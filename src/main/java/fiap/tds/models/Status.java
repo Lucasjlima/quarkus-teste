2acc1d5be7f47a77afff9f304b1b30cc69220160
Lucas <lucasjlima2006@gmail.com>
2025-05-19 21:27:29 -0300

    adicionando mais um status SOLICITAR_AJUDA

--- src/main/java/fiap/tds/models/Status.java
+++ src/main/java/fiap/tds/models/Status.java
@@ -3,8 +3,10 @@ package fiap.tds.models;
 public enum Status {
     EM_ANDAMENTO,
     SEM_RESPOSTA,
-    FINALIZADO;
+    FINALIZADO,
+    AJUDA_SOLICITADA;
 
+    // I need to add a new status called "AJUDA_SOLICITADA"
     Status() {
     }
 
