8db66a81aba1d939e63dd305c0e3369ad466e4b8
Lucas <lucasjlima2006@gmail.com>
2025-05-21 10:11:26 -0300

    adicionando a rota POST para solicitar ajuda

--- src/main/java/fiap/tds/resource/SolicitHelpResource.java
+++ src/main/java/fiap/tds/resource/SolicitHelpResource.java
@@ -6,10 +6,9 @@ import fiap.tds.models.Status;
 import fiap.tds.models.TypeEvent;
 import fiap.tds.services.EventService;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,18 +21,6 @@ public class SolicitHelpResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    //This method will return the list of events that only has the status EM_ANDAMENTO
-    //and the position that is responsible for the event
-    /*
-    public List<HelpEventDTO> getAllEvents() {
-        var events = eventService.getAllEvents();
-        return events.stream()
-                .filter(e -> e.getStatus().equals(Status.EM_ANDAMENTO))
-                .map(e -> new HelpEventDTO(e.getId(), e.getTypeEvent(), e.getPosition()))
-                .collect(Collectors.toList());
-    }
-
-     */
     public List<HelpEventDTO> listEvents(){
         var events = eventService.getAllEvents();
         var eventsFiltered = events.stream()
@@ -43,4 +30,15 @@ public class SolicitHelpResource {
                 .map(e -> new HelpEventDTO( e.getTypeEvent().name(), e.getPosition(), e.getStatus()))
                 .collect(Collectors.toList());
     }
+
+    //I need to create a PUT method to update the status of some event to AJUDA_SOLICITADA
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Response requestHelp(@PathParam("id") Long id){
+        eventService.requestHelp(id);
+        return Response.ok( "Status do evento foi atualizado com sucesso!").build();
+    }
+
 }
