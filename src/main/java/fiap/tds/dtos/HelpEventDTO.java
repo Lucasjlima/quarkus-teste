ff2d59f7663d2ab4c526904d6c0ef79342bb0c29
Lucas <lucasjlima2006@gmail.com>
2025-05-22 10:00:26 -0300

    Colocando id como atributo

--- src/main/java/fiap/tds/dtos/HelpEventDTO.java
+++ src/main/java/fiap/tds/dtos/HelpEventDTO.java
@@ -3,6 +3,7 @@ package fiap.tds.dtos;
 import fiap.tds.models.Status;
 
 public class HelpEventDTO {
+    private long id;
     private String type;
     private String position;
     private Status status;
@@ -10,10 +11,19 @@ public class HelpEventDTO {
     public HelpEventDTO() {
     }
 
-    public HelpEventDTO(String type, String position, Status status) {
+    public HelpEventDTO(long id, String type, String position, Status status) {
+        this.id = id;
         this.type = type;
         this.position = position;
         this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getType() {
